e8daa92bd89c9ee859e3508d2fc2d73c4457623a
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-12-13 18:38:27 -0300

    atualizacao

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pyautogui
 from time import sleep
 
 for i in range(50):
-    pyautogui.click(1834,305, duration=3);
-    pyautogui.click(2010,436, duration=4);
-    pyautogui.click(2115,530, duration=4);
-    pyautogui.click(2281,334, duration=3);
+    pyautogui.click(1834,305, duration=3)
+    pyautogui.click(2010,436, duration=3)
+    pyautogui.click(2115,530, duration=3)
+    pyautogui.click(2281,334, duration=3)
+    sleep(3)
