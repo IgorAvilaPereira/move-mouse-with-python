283838b2a1d05a936078b7c9c3779b0883ca01f8
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-12-14 11:50:18 -0300

    notas

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pyautogui
 from time import sleep
 
-for i in range(50):
+for i in range(500):
     pyautogui.click(1834,305, duration=3)
-    pyautogui.click(2010,436, duration=3)
-    pyautogui.click(2115,530, duration=3)
-    pyautogui.click(2281,334, duration=3)
-    sleep(3)
+    pyautogui.click(2012,471, duration=5)    
+    pyautogui.click(2072,580, duration=3)    
+    pyautogui.click(2255,334, duration=3)    
